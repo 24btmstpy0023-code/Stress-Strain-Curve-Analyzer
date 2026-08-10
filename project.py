d97eb03d4864f94b895d453521b22f217dba9867
Priyaranjan Sakthivel <24btmstpy0023@pondiuni.ac.in>
2026-08-10 18:56:06 +0530

    Update project.py

--- project.py
+++ project.py
@@ -20,13 +20,13 @@ if file:
     df = pd.read_csv(file,header=None,names=["strain","stress"])
     strain = df["strain"]
     stress = df["stress"]
-    offset_stress = E*(strain - 0.002)
+   # offset_stress = E*(strain - 0.002)
     st.write("### Uploaded Data")
     st.dataframe(df)
     # Plot Graph
     fig, ax = plt.subplots()
     ax.plot(strain, stress)
-    ax.plot(strain,offset_stress,linestyle = "dashed",color ="k",label="offset_line")
+   # ax.plot(strain,offset_stress,linestyle = "dashed",color ="k",label="offset_line")
     ax.axhline(max(stress),linestyle="dashed",color = "red",label="UTS")
     ax.axvline(max(strain),limestyle="dashed",color = "green",label ="Max strain")
     ax.set_xlabel("Strain")
