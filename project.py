c5c7cd4120b709fdd717852fdbd834caef991742
Priyaranjan Sakthivel <24btmstpy0023@pondiuni.ac.in>
2026-08-10 20:19:03 +0530

    Update project.py

--- project.py
+++ project.py
@@ -7,7 +7,7 @@ st.title("Stress-Strain Analyzer - by Priyaranjan")
 # Young's Modulus Input
 E = st.number_input(
     "Enter Young's Modulus (MPa)",
-    value=69000)
+    value=31763)
 
 offset = 0.002
 # Upload CSV
